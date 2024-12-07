cb1b0c1909fa9e671a3fc9b9b2aab9632e5296ed
Leo <eta.development.leo@outlook.com>
2024-12-07 09:04:05 +0300

    update packages

--- src/main-rendered-window.tsx
+++ src/main-rendered-window.tsx
@@ -1,14 +1,14 @@
 import './index.css';
 import 'devextreme/dist/css/dx.material.orange.dark.css';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { App } from './app/app';
 
 const container = document.getElementById('app-container');
+const root = createRoot(container);
 
-render(
+root.render(
   <HashRouter>
     <App />
-  </HashRouter>,
-  container
+  </HashRouter>
 );
